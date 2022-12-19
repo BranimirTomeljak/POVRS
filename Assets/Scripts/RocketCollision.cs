32f9dc6a3adaf479074266e6a5c19e7e808a58c4
BranimirTomeljak <mtomeljak@gmail.com>
2022-12-19 17:52:27 +0100

    solved 4, 10, 11, 13

--- Assets/Scripts/RocketCollision.cs
+++ Assets/Scripts/RocketCollision.cs
@@ -2,21 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class RocketCollision : MonoBehaviour
 {
     public GameObject target;
+    public GameObject launcher;
+    public AudioClip explosionSound;
+    public bool hit = false;
+    
     void Awake()
     {
         target = GameObject.Find("Tank");
+        launcher = GameObject.Find("RocketSpawnPoint");
     }
 
     void OnCollisionEnter(Collision collisionInfo)
     {
         Debug.Log(collisionInfo.collider.transform);
-        if(collisionInfo.collider.tag == "Target"){
-            Debug.Log("pogodio tenk!");
 
+        if(collisionInfo.collider.tag == "Target" && hit == false){
+            var soundPosition = new Vector3(target.transform.position.x + 15f, target.transform.position.y + 15f, target.transform.position.z + 15f);
+            AudioSource.PlayClipAtPoint(explosionSound, soundPosition);
+            Debug.Log("pogodio tenk!");
+            hit = true;
             Destroy(target);
+        }
+        else{
+            AudioSource.PlayClipAtPoint(explosionSound, launcher.transform.position);
         }
     }
 }
--- Assets/Scripts/RocketLauncher.cs
+++ Assets/Scripts/RocketLauncher.cs
@@ -4,29 +4,45 @@ using UnityEngine;
 
 public class RocketLauncher : MonoBehaviour
 {
+    public GameObject cameraPosition;
     public GameObject rocketPrefab;
     public GameObject spawnPosition;
-    public GameObject target;
+    public RaycastHit target;
     public float speed = 20f;
+
+    private void Start(){
+        cameraPosition.transform.position = spawnPosition.transform.position;
+    }
 
     // Update is called once per frame
     private void Update()
     {
         if(Input.GetMouseButtonDown(0))
         {
-            GameObject rocket = Instantiate(rocketPrefab, spawnPosition.transform.position, rocketPrefab.transform.rotation);
-            rocket.transform.LookAt(target.transform);
-            StartCoroutine(SendHoming(rocket));
+            Ray ray = Camera.main.ScreenPointToRay( Input.mousePosition );
+            target = new RaycastHit();
+         
+            if( Physics.Raycast( ray, out target, 999999 ) )
+            {
+                GameObject rocket = Instantiate(rocketPrefab, spawnPosition.transform.position, rocketPrefab.transform.rotation);
+                rocket.transform.LookAt(target.point);
+                StartCoroutine(SendHoming(rocket));
+            }
         }
     }
 
     public IEnumerator SendHoming(GameObject rocket)
     {
         //ako je GameObject.Find("Tank") == null onda je tenk unisten u skripti RocketCollision.cs
-        while(GameObject.Find("Tank") != null && Vector3.Distance(target.transform.position, rocket.transform.position) > 0.3f)
+        while(GameObject.Find("Tank") != null && Vector3.Distance(target.point, rocket.transform.position) > 0.3f)
         {
-            rocket.transform.position += (target.transform.position - rocket.transform.position).normalized * speed * Time.deltaTime;
-            rocket.transform.LookAt(target.transform);
+            if(target.transform.gameObject.tag == "Target" && Vector3.Distance(target.point, rocket.transform.position) < 25f){
+                cameraPosition.transform.position = new Vector3(target.point.x + 15f, target.point.y + 15f, target.point.z + 15f);
+                cameraPosition.transform.LookAt(target.point);
+            }
+
+            rocket.transform.position += (target.point - rocket.transform.position).normalized * speed * Time.deltaTime;
+            rocket.transform.LookAt(target.point);
             yield return null;
         }
         
