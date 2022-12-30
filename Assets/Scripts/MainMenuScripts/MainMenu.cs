3cd2217a6fe26a859633710cbc9474545e7369d5
petramazar <pm53371@fer.hr>
2022-12-30 16:15:51 +0100

    uređena scena nakon simulacije

--- Assets/Scripts/MainMenuScripts/MainMenu.cs
+++ Assets/Scripts/MainMenuScripts/MainMenu.cs
@@ -12,7 +12,12 @@ public class MainMenu : MonoBehaviour
     }
 
     public void PlayGame(){
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        if (SceneManager.GetActiveScene().buildIndex + 1 == 3) {
+           SceneManager.LoadScene(1); 
+        } else {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        
     }
 
     public void QuitGame(){
--- Assets/Scripts/RocketCollision.cs
+++ Assets/Scripts/RocketCollision.cs
@@ -38,7 +38,7 @@ public class RocketCollision : MonoBehaviour
             Instantiate(explosionTank, target.transform.position, Quaternion.identity);
             Destroy(target);
             StartCoroutine(NewHeading());
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
         else{
             if(cameraEnd.enabled && collisionInfo.collider.tag != "Target"){    //u slucaju da se kamera pomakne, a ipak ne pogodi metu
@@ -63,7 +63,7 @@ public class RocketCollision : MonoBehaviour
     IEnumerator NewHeading () {
 		while (true) {
 			//NewHeadingRoutine();
-			yield return new WaitForSeconds(2.0f);
+			yield return new WaitForSeconds(3.0f);
 		}
 	}
 }
