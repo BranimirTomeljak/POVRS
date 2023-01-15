1685cf0f3d23f42d2a2f089f8fdcfb2eff75969f
BranimirTomeljak <mtomeljak@gmail.com>
2023-01-15 01:23:34 +0100

    settings calibration

--- Assets/Scripts/EnemyBehaviorSimulator.cs
+++ Assets/Scripts/EnemyBehaviorSimulator.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnemyBehaviorSimulator : MonoBehaviour
 {
     public GameObject launcher;
-    public ParticleSystem explosionElse;
+    public ParticleSystem explosion;
     public AudioClip explosionSound;
     public float randomTime = 2.0f;
     public float elapsedTime = 0.0f;
@@ -23,20 +23,18 @@ public class EnemyBehaviorSimulator : MonoBehaviour
     {
         elapsedTime += Time.deltaTime;
         if(elapsedTime > randomTime && PlayerPrefs.GetString("enemyBehaviorSelected") == "Neprijatelj dinamičan"){
-            randomTime = Random.Range(1.0f, 4.0f);
-            Debug.Log("!!!!: " + randomTime);
+            randomTime = Random.Range(1.0f, 3.0f);
             elapsedTime = 0;
 
             Vector3 randomSpawnPosition = launcher.transform.position;
-            while(Vector3.Distance(randomSpawnPosition, launcher.transform.position) < 5.0f){
-                randomSpawnPosition = new Vector3(Random.Range(launcher.transform.position.x - 30, launcher.transform.position.x + 30), 20, Random.Range(launcher.transform.position.z - 30, launcher.transform.position.z + 30));
+            while(Vector3.Distance(randomSpawnPosition, launcher.transform.position) < 7.0f){
+                randomSpawnPosition = new Vector3(Random.Range(launcher.transform.position.x - 60, launcher.transform.position.x + 60), 60, Random.Range(launcher.transform.position.z - 60, launcher.transform.position.z + 60));
             }            
 
             RaycastHit hit;
             if(Physics.Raycast(randomSpawnPosition, Vector3.down, out hit)){
-                Debug.Log(hit.point);
                 AudioSource.PlayClipAtPoint(explosionSound, hit.point);
-                Instantiate(explosionElse, hit.point, Quaternion.identity);
+                Instantiate(explosion, hit.point, Quaternion.identity);
             }
         }
     }
