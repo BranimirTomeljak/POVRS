f203fd04c8ec5721fea6cfb82b9e2fd8a04469ed
BranimirTomeljak <mtomeljak@gmail.com>
2023-01-15 17:00:33 +0100

    fix position0 error

--- Assets/Scripts/StartPositioning.cs
+++ Assets/Scripts/StartPositioning.cs
@@ -7,11 +7,17 @@ public class StartPositioning : MonoBehaviour
     {
         GameObject tank = GameObject.Find("Tank");
         GameObject POVRS = GameObject.Find("POVRS");
+
+        int num = Random.Range(0,10);
         string [] positions = txtFile.text.Split('\n');
-        string [] row = positions[Random.Range(0,10)].Split(',');
+        string [] row = positions[num].Split(',');
+
+        tank.transform.position = new Vector3(float.Parse(row[0]), float.Parse(row[1]), float.Parse(row[2]));
         
-        tank.transform.position = new Vector3(float.Parse(row[0]),float.Parse(row[1]),float.Parse(row[2]));
-        POVRS.transform.position = new Vector3(float.Parse(row[3]),float.Parse(row[4]),float.Parse(row[5]));
+        if(num == 0)
+            POVRS.transform.position = new Vector3(float.Parse(row[3]), 0.4f, float.Parse(row[5]));
+        else
+            POVRS.transform.position = new Vector3(float.Parse(row[3]), float.Parse(row[4]), float.Parse(row[5]));
 
         POVRS.transform.LookAt(tank.transform);
     }
